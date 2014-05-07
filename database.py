d9dacc205c75d922db144a1b799bf02d837e21ee
devildeveloper <devx.cry@gmail.com>
2014-05-07 16:31:40 -0500

    probando la validacion de vigencia de enlaces --commit con errores

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Date, String, Integer, ForeignKey , func
+from sqlalchemy import Column, DateTime, Date,String, Integer, ForeignKey , func
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-from datetime import date
+import datetime
 Base=declarative_base()
 
 class User(Base):
@@ -18,7 +18,7 @@ class File(Base):
 	id =Column(Integer,primary_key=True)
 	name=Column(String)
 	hired_on=Column(Date,default=func.now())
-	expiration=Column(String)
+	expiration=Column(Date,default=(datetime.datetime.today()+datetime.timedelta(weeks=1)).date())
 	status=Column(Integer,default=1)#0 = user disabled	
 	user_id=Column(Integer,ForeignKey('user.id'))
 	user =relationship(
